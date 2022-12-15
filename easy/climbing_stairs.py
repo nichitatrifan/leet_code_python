c202d3eb6711f5ed4aa59253233b60da6b7879b8
Nichita Trifan <nikiton254@gmail.com>
2022-12-14 18:25:07 -0600

    climbing stair problem

--- easy/climbing_stairs.py
+++ easy/climbing_stairs.py
@@ -3,8 +3,19 @@
 # In how many distinct ways can you climb to the top?
 
 class Solution:
+    memory= {0 : 0}
+    def climbStairs(self, n: int) -> int:
+        if n in self.memory:
+            return self.memory[n]
+        elif n - 1 == 0 or n - 2 == 0:
+            return n
+        else:
+            self.memory[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.memory[n]
+
+
     paths = 0
-    def climbStairs2(self, n: int)-> int:
+    def climbStairs_inefficient(self, n: int)-> int:
         if n < 0:
             #print("path is incorrect")
             return n
@@ -12,11 +23,11 @@ class Solution:
             self.paths += 1
             #print("path is correct")
             return 0
-        self.climbStairs2(n - 1)
-        self.climbStairs2(n - 2)
+        self.climbStairs_inefficient(n - 1)
+        self.climbStairs_inefficient(n - 2)
         return self.paths
 
-    def climbStairs(self, n: int): #-> int:
+    def climbStairs2(self, n: int): #-> int:
         """finds the best path to climb the stairs
 
         Args:
@@ -26,12 +37,12 @@ class Solution:
             print("0")
         elif n - 2 >= 0:
             print("-2")
-            self.climbStairs(n - 2)
+            self.climbStairs2(n - 2)
         elif n - 1 >= 0:
             print("-1")
-            self.climbStairs(n - 1)
+            self.climbStairs2(n - 1)
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.climbStairs2(38))
+    print(solution.climbStairs(38))
